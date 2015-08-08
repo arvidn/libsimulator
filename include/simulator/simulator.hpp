02cd18cee467e9dc27f576138641a0f778d7dc71
arvidn <arvid@cs.umu.se>
2015-08-08 19:31:00 -0400

    fix typo in libsimulator header

--- include/simulator/simulator.hpp
+++ include/simulator/simulator.hpp
@@ -148,16 +148,12 @@ namespace sim
 #if defined BOOST_ASIO_HAS_STD_CHRONO
 		typedef std::nano period;
 		typedef std::chrono::time_point<high_resolution_clock
-			, std::chrono::nanoseconds>
-			time_point;
-		typedef std::chrono::duration<boost::int64_t, std::nano>
-			duration;
+			, std::chrono::nanoseconds> time_point;
+		typedef std::chrono::duration<boost::int64_t, std::nano> duration;
 #else
 		typedef boost::nano period;
-		typedef time_point<high_resolution_clock, boost::nano>
-			time_point;
-		typedef duration<boost::int64_t, boost::nano
-			duration;
+		typedef time_point<high_resolution_clock, boost::nano> time_point;
+		typedef duration<boost::int64_t, boost::nano> duration;
 #endif
 		static const bool is_steady = true;
 		static time_point now();
